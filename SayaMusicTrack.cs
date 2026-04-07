7c50b50ce4a34b6774e54b5969b82fb332797b49
yohanabarus887 <yohanabarus887@gmail.com>
2026-04-07 08:11:48 +0700

    menambahkan dua class SyaMusicUser dan SayaMusicTrack

---
+++ SayaMusicTrack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Text;
+
+namespace Modul6_103022400139
+{
+    internal class SayaMusicTrack
+    {
+        private int id;
+        public string title;
+        private int playCount;
+
+        public SayaMusicTrack(string title)
+        {
+            Contract.Requires(title.Length <= 200, "Title melebihi 200 kata");
+
+            Random rand = new Random();
+
+            this.id = rand.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            playCount += count;
+        }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Play Count: " + playCount);
+        }
+    }
+}
---
+++ SayaMusicUser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Text;
+
+namespace Modul6_103022400139
+{
+    internal class SayaMusicUser
+    {
+        private int id;
+        public string Username;
+        private List<SayaMusicTrack> uploadedTracks;
+
+        public SayaMusicUser(string title)
+        {
+
+            Random rand = new Random();
+
+            this.id = rand.Next(10000, 99999);
+            this.Username = Username;
+            this.uploadedTracks = new List<SayaMusicTrack>();
+        }
+
+        public void GetTotalPlayCount()
+        {
+        }
+
+        public void AddTrack(SayaMusicTrack track)
+        {
+
+        }
+
+        public void PrintAllTracks()
+        {
+
+        }
+    }
+}
